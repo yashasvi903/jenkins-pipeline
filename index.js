62c2a98c9eb2d1491aea14902dceb4b3d100c9b4
Yashasvi Yadav <yashasvi.yadav@unibots.com>
2025-10-23 13:08:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import { createServer } from 'http';
+
+const PORT = process.env.PORT || 3000;
+
+const server = createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello from Node.js app!\n');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
