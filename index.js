84469c2ab1dd3c3de8749dfb467490c80cab5631
Yashasvi Yadav <yashasvi.yadav@unibots.com>
2025-10-23 15:29:25 +0530

    build hoja

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import { createServer } from 'http';
 const PORT = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello from Node.js app!\n');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from Node.js app! Jenkins auto build test ✅\n');
 });
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    console.log(`this is a test for automate`)
+    console.log(`🚀 Jenkins automation test triggered!`);
 });
